0f826cc283a6120f967680e83e0c9006e843f887
codeworkx <daniel.hillenbrand@codeworkx.de>
2012-05-31 16:08:58 +0200

    gadget: disabled samsung mtp and composite, extended google code

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -605,23 +605,34 @@ static int mass_storage_function_init(struct android_usb_function *f,
 		}
 	} else {
 #endif
-		/* original mainline code */
-	config->fsg.nluns = 1;
-	config->fsg.luns[0].removable = 1;
+        // default number of luns
+        config->fsg.nluns = 2;
+        for (i = 0; i < config->fsg.nluns; i++) {
+            config->fsg.luns[i].removable = 1;
+            config->fsg.luns[i].nofua = 1;
+        }
 
-	common = fsg_common_init(NULL, cdev, &config->fsg);
-	if (IS_ERR(common)) {
-		kfree(config);
-		return PTR_ERR(common);
-	}
+        common = fsg_common_init(NULL, cdev, &config->fsg);
+        if (IS_ERR(common)) {
+            kfree(config);
+            return PTR_ERR(common);
+        }
 
-	err = sysfs_create_link(&f->dev->kobj,
-				&common->luns[0].dev.kobj,
-				"lun");
-	if (err) {
-		kfree(config);
-		return err;
-	}
+        for (i = 0; i < config->fsg.nluns; i++) {
+            char luns[5];
+            err = snprintf(luns, 5, "lun%d", i);
+            if (err == 0) {
+                printk(KERN_ERR "usb: %s snprintf error\n", __func__);
+                return err;
+            }
+            err = sysfs_create_link(&f->dev->kobj,
+                        &common->luns[i].dev.kobj,
+                        luns);
+            if (err) {
+                kfree(config);
+                return err;
+            }
+		}
 #ifdef CONFIG_USB_ANDROID_SAMSUNG_COMPOSITE
 	}
 #endif
