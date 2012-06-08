dd08d51e8a0e93ceb0eb4d2392ff9ba56c1a4170
codeworkx <daniel.hillenbrand@codeworkx.de>
2012-06-08 20:33:21 +0200

    omap_hsmmc: removed cap to perform an mmc erase
    Change-Id: I2d26a7a095f786ac247db64c1e0ea9c8edb3cb7a

--- drivers/mmc/host/omap_hsmmc.c
+++ drivers/mmc/host/omap_hsmmc.c
@@ -2386,8 +2386,11 @@ static int __init omap_hsmmc_probe(struct platform_device *pdev)
 	mmc->max_req_size = mmc->max_blk_size * mmc->max_blk_count;
 	mmc->max_seg_size = mmc->max_req_size;
 
+/*
+ * MMC_CAP_ERASE removed for security reasons
+ */
 	mmc->caps |= MMC_CAP_MMC_HIGHSPEED | MMC_CAP_SD_HIGHSPEED |
-		     MMC_CAP_WAIT_WHILE_BUSY | MMC_CAP_ERASE;
+		     MMC_CAP_WAIT_WHILE_BUSY;
 
 	mmc->caps |= mmc_slot(host).caps;
 	if (mmc->caps & MMC_CAP_8_BIT_DATA)
