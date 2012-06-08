df5d96fecb4b7489f2f9713d733a9babfb17e95a
codeworkx <daniel.hillenbrand@codeworkx.de>
2012-06-08 14:14:31 +0200

    cypress: force autocalib, fixes menu key after suspend/resume
    Change-Id: I1ea29d2a99538f01cfb685526ba51f7302613426

--- drivers/input/keyboard/cypress-touchkey.c
+++ drivers/input/keyboard/cypress-touchkey.c
@@ -101,13 +101,11 @@ static int cptk_i2c_write(struct cptk_data *cptk, u8 cmd,
     struct i2c_msg msg[1];
     int retry = 2;
 
-
+    mutex_lock(&cptk->i2c_lock);
     if (!cptk->enable) {
         pr_err("cptk :key is not enabled.\n");
         return -ENODEV;
     }
-
-    mutex_lock(&cptk->i2c_lock);
 
     while (retry--) {
         data[0] = cmd;
@@ -121,8 +119,7 @@ static int cptk_i2c_write(struct cptk_data *cptk, u8 cmd,
             mutex_unlock(&cptk->i2c_lock);
             return 0;
         }
-        pr_debug("cptk :i2c transfer retor for cmd:%d\n",
-               cmd);
+        pr_info("cptk :i2c transfer retry for cmd:%d\n", cmd);
         msleep(20);
     }
 
@@ -255,7 +252,7 @@ static int cptk_early_suspend(struct early_suspend *h)
 
 static int cptk_late_resume(struct early_suspend *h)
 {
-    pr_debug("cptk: %s\n", __func__);
+    pr_info("cptk: %s\n", __func__);
 
     struct cptk_data *cptk = container_of(h,
             struct cptk_data,
@@ -267,11 +264,11 @@ static int cptk_late_resume(struct early_suspend *h)
             cptk->pdata->power(1);
         cptk->enable = true;
         enable_irq(cptk->client->irq);
-
-        cptk_i2c_write(cptk, KEYCODE_REG, AUTO_CAL_MODE_CMD);
-        cptk_i2c_write(cptk, CMD_REG, AUTO_CAL_EN_CMD);
-        msleep(50);
     }
+    pr_info("cptk: %s auto calibration...\n", __func__);
+    cptk_i2c_write(cptk, KEYCODE_REG, AUTO_CAL_MODE_CMD);
+    cptk_i2c_write(cptk, CMD_REG, AUTO_CAL_EN_CMD);
+    msleep(50);
 
     if (cptk->enable) {
         if (touch_led_disabled == 0) {
@@ -837,6 +834,7 @@ static int __devinit cptk_i2c_probe(struct i2c_client *client,
 
     if (cptk->pdata->power)
         cptk->pdata->power(1);
+
     cptk->enable = true;
     cptk->notification = false;
 
@@ -862,6 +860,7 @@ static int __devinit cptk_i2c_probe(struct i2c_client *client,
     register_early_suspend(&cptk->early_suspend);
 #endif
 
+    pr_info("cptk: %s first auto calibration...\n", __func__);
     cptk_i2c_write(cptk, KEYCODE_REG, AUTO_CAL_MODE_CMD);
     cptk_i2c_write(cptk, CMD_REG, AUTO_CAL_EN_CMD);
 
@@ -869,22 +868,34 @@ static int __devinit cptk_i2c_probe(struct i2c_client *client,
                    IRQF_TRIGGER_LOW | IRQF_ONESHOT,
                    DEVICE_NAME, cptk);
 
+    if (ret < 0) {
+        pr_err("%s: request_threaded_irq returned %d\n", __func__, ret);
+        goto err_exit2;
+    }
+
     // init workqueue
     cptk->wq = create_singlethread_workqueue("cptk_wq");
     if (!cptk->wq) {
         ret = -ENOMEM;
         pr_err("%s: could not create workqueue\n", __func__);
+        goto err_exit2;
     }
 
     /* this is the thread function we run on the work queue */
 	INIT_WORK(&cptk->work, touch_led_timedout_work);
 
-    if (ret < 0)
-        goto err_exit2;
-
     ret = cptk_create_sec_touchkey(cptk);
-    if (ret < 0)
+    if (ret < 0) {
+        pr_err("%s: cptk_create_sec_touchkey returned %d\n", __func__, ret);
         goto err_exit2;
+    }
+
+    /* trigger autocalibration again to make sure.
+     * first one seems to fail on some phones.
+     */
+    pr_info("cptk: %s second auto calibration...\n", __func__);
+    cptk_i2c_write(cptk, KEYCODE_REG, AUTO_CAL_MODE_CMD);
+    cptk_i2c_write(cptk, CMD_REG, AUTO_CAL_EN_CMD);
 
     return 0;
 
